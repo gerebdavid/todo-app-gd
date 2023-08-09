5f9a016db99431316f329d2a6590701d4571c34d
David Gereb <gereb@webgurus.io>
2023-08-09 16:46:19 +0300

    ITL-0 Created separate components, also added absolute paths

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,25 @@
+<template>
+    <button @click="clearTodos">Clear</button>
+    <TodoForm @addTodo="addTodo" />
+    <TodoList v-if="todos.length" :todos="todos" @removeTodo="removeTodo" />
+</template>
+
+<script setup lang="ts">
+import { Ref, ref } from "vue"
+import TodoForm from "@/components/TodoForm.vue"
+import TodoList from "@/components/TodoList.vue"
+
+const todos: Ref<string[]> = ref([])
+
+function addTodo(todo: string) {
+    todos.value.push(todo)
+}
+
+function removeTodo(index: number) {
+    todos.value.splice(index, 1)
+}
+
+function clearTodos() {
+    todos.value = []
+}
+</script>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,11 +1,5 @@
 <template>
-    <button @click="clearTodos">Clear</button>
-    <form>
-        <input v-model="todo" />
-        <button @click.prevent="addTodo">Submit</button>
-    </form>
-
-    <ul v-if="todos.length">
+    <ul>
         <div v-for="(todo, index) in todos" :key="index" class="my-2 flex items-center gap-x-2">
             <li>
                 {{ todo }}
@@ -15,26 +9,16 @@
     </ul>
 </template>
 
-<script lang="ts" setup>
-import { Ref, ref } from "vue"
-
-const todo = ref("")
-const todos: Ref<string[]> = ref([])
-
-function addTodo() {
-    if (!todo.value) {
-        return
-    }
-
-    todos.value.push(todo.value)
-    todo.value = ""
+<script setup lang="ts">
+interface Props {
+    todos: string[]
 }
+
+defineProps<Props>()
+
+const emit = defineEmits(["removeTodo"])
 
 function removeTodo(index: number) {
-    todos.value.splice(index, 1)
-}
-
-function clearTodos() {
-    todos.value = []
+    emit("removeTodo", index)
 }
 </script>
