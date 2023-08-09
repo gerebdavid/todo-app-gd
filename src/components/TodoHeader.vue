4806a66c358c06a399f2ffc15cfdb43656a007b6
David Gereb <gereb@webgurus.io>
2023-08-09 17:19:49 +0300

    ITL-1 Implement Page header

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex justify-between">
+        <h1>To do list</h1>
+        <button class="flex h-16 w-16 items-center justify-center rounded-full bg-green" @click="addTodo">
+            <PlusIcon class="bg-yellow fill-white" />
+        </button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import PlusIcon from "@/components/icons/PlusIcon.vue"
+
+const emit = defineEmits(["addTodo"])
+
+function addTodo() {
+    emit("addTodo")
+}
+</script>
